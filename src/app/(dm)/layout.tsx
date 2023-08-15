de374d3f020e60a6659f6fae239982da54002b39
Igor Penaque <igor.penaque@sysmap.com.br>
2023-08-15 13:07:49 -0300

    fix: enforcing dm layout revalidation

--- src/app/(dm)/layout.tsx
+++ src/app/(dm)/layout.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import DMLayoutSkeleton from "@/components/islets/dm-layout/dm-layout-skeleton";
 import DMLayout from "@/components/islets/dm-layout";
 
+export const revalidate = 0;
+
 export default function SuspendedDMLayout({
   children,
 }: {
