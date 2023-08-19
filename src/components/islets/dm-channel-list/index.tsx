d92cbb1870a0da5b3306bfcaf0898f64dad75beb
Kamil Drozdz <kamil12x@wp.pl>
2023-08-19 13:07:03 +0200

    adding ficzera - removing from the list of channels that the user does not want to have conversations.

--- src/components/islets/dm-channel-list/index.tsx
+++ src/components/islets/dm-channel-list/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React from "react";
 import { ListedDMChannel } from "@/lib/entities/channel";
 import { List } from "@/components/ui/list";
 import DMChannelListHeader from "./dm-channel-list-header";
@@ -6,13 +8,26 @@ import DMChannelListItem from "./dm-channel-list-item";
 interface DMChannelListrops {
   channels: ListedDMChannel[];
 }
-export default async function DMChannelList({ channels }: DMChannelListrops) {
+export default function DMChannelList({ channels }: DMChannelListrops) {
+  const [currentChannels, setCurrentChannels] = React.useState(channels);
+
+  const handleChannelDelete = (channelId: string) => {
+    const updatedChannels = currentChannels.filter(
+      (channel) => channel.id !== channelId,
+    );
+    setCurrentChannels(updatedChannels);
+  };
+
   return (
     <div className="pt-4">
       <DMChannelListHeader />
       <List className="mt-1">
-        {channels.map((channel) => (
-          <DMChannelListItem key={channel.id} channel={channel} />
+        {currentChannels.map((channel) => (
+          <DMChannelListItem
+            key={channel.id}
+            channel={channel}
+            onDelete={() => handleChannelDelete(channel.id)}
+          />
         ))}
       </List>
     </div>
