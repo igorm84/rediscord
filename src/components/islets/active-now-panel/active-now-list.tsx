6c113f167bf4817fe8c869a84b126358ec1f388c
Kamil Drozdz <kamil12x@wp.pl>
2023-08-24 20:02:56 +0200

    feat: adding a message option like on discord in the "Active now" tab on the hover, or a profile where the profile is currently not clickable

--- src/components/islets/active-now-panel/active-now-list.tsx
+++ src/components/islets/active-now-panel/active-now-list.tsx
@@ -1,16 +1,27 @@
 "use client";
 import Avatar from "@/components/ui/avatar";
+import Divider from "@/components/ui/divider";
 import { List, ListItem } from "@/components/ui/list";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { ActivityTypes } from "@/lib/entities/activity";
 import { UserStatuses } from "@/lib/entities/user";
+import { useChannelStore } from "@/state/channel-list";
 import { useFriendStore } from "@/state/friend-list";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export const ActiveNowListItemSkeleton = () => (
   <div className="h-[70px] animate-pulse rounded-md bg-gray-900"></div>
 );
 export default function ActiveNowList() {
   const { friends } = useFriendStore();
-
+  const { channels, setChannels } = useChannelStore();
+  const router = useRouter();
   if (friends === null) {
     return <ActiveNowListItemSkeleton />;
   }
@@ -21,31 +32,79 @@ export default function ActiveNowList() {
       friend.activity.type === ActivityTypes.Playing &&
       friend.status !== UserStatuses.Offline,
   );
+  const handleAddChannel = (friend) => {
+    if (channels !== null) {
+      const isFriendAlreadyAdded = channels.some(
+        (channel) => channel.id === friend.id,
+      );
+      if (!isFriendAlreadyAdded) {
+        setChannels([friend, ...channels]);
+      }
+      router.push(`/channels/${friend.id}`);
+    }
+  };
 
   return filteredList.length ? (
     <List>
       {filteredList.map((friend) => (
-        <ListItem
-          key={friend.id}
-          noVerticalPadding
-          href={`/channels/${friend.id}`}
-          className="group gap-3 border-[1px] border-gray-800  bg-midground p-4"
-        >
-          <Avatar
-            src={friend.avatar}
-            alt={friend.name}
-            status={friend.status}
-            className="w-8 flex-none"
-          />
-          <div className="flex-1 truncate text-sm">
-            <span className="text-gray-100">{friend.name}</span>
-            {friend.activity && (
-              <div className="h-4 truncate text-xs leading-3">
-                {friend.activity.name} - 5 hours
+        <TooltipProvider key={friend.id}>
+          <Tooltip>
+            <TooltipTrigger className="w-full">
+              <ListItem
+                noVerticalPadding
+                className="group  gap-3 border-[1px] border-gray-800  bg-midground p-4"
+              >
+                <Avatar
+                  src={friend.avatar}
+                  alt={friend.name}
+                  status={friend.status}
+                  className="w-8 flex-none"
+                />
+                <div className="flex-1 truncate text-sm">
+                  <span className="text-gray-100">{friend.name}</span>
+                  {friend.activity && (
+                    <div className="h-4 truncate text-xs leading-3">
+                      {friend.activity.name} - 5 hours
+                    </div>
+                  )}
+                </div>
+                <Image
+                  className=" aspect-square  object-contain"
+                  width={24}
+                  height={24}
+                  alt={`logo {friend.activity.name} `}
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlvnzzC2oPp5U3HelWw286q48kMHcsjW9rC7Tlni7T-0amCpHLvDiqZVidP2Ony0X7tSA&usqp=CAU"
+                />
+              </ListItem>
+            </TooltipTrigger>
+            <TooltipContent
+              side="left"
+              sideOffset={15}
+              className="p-2 text-xs leading-3 md:w-[200px]"
+            >
+              <div className="flex w-full flex-col items-end justify-center ">
+                <p
+                  onClick={() => {
+                    handleAddChannel(friend);
+                  }}
+                  className="flex h-8 w-full cursor-pointer items-center rounded px-2 transition-colors duration-300 ease-in-out hover:bg-blue-600"
+                >
+                  Message
+                </p>
+                <Divider className="my-1 w-full" />
+                <div className="flex w-full items-center rounded  px-2 transition-colors duration-300 ease-in-out hover:bg-blue-600">
+                  <Avatar
+                    src={friend.avatar}
+                    alt={friend.name}
+                    status={friend.status}
+                    className="mr-2 !w-6 flex-none"
+                  />
+                  <p> {friend.name}</p>
+                </div>
               </div>
-            )}
-          </div>
-        </ListItem>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       ))}
     </List>
   ) : (
